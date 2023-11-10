60c025e96df5d4d190c65c26acdc5f6283b3a6fa
AruzhanIssabekova <aruzhan.issabekova123@gmail.com>
2023-11-11 01:13:39 +0600

    add HW2

---
+++ HW2/HW2.py
@@ -0,0 +1,21 @@
+# a=input('Имя:')
+# b=input('Фамилия:')
+# c=input('Отчество:')
+# d=input('Возраст:')
+# print(f'Меня зовут {b} {a} {c}. Мне {d} лет и я хочу у Вас работать.')
+
+# a=int(input())
+# b=int(input())
+# N=a//b
+# M=a%b
+# print(f'Целая часть от деления {N} и остаток {M}')
+
+# str1=input()
+# str2=input()
+# print(str1==str2)
+
+# MULTIPLE_VAL=6.4
+# M=int(input())
+# print(M*MULTIPLE_VAL)
+# print(int(M*MULTIPLE_VAL))
+# print(str(M*MULTIPLE_VAL))
